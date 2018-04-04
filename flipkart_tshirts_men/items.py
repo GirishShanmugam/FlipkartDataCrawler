030c04b93e108efcb9ddb67b98f9d60924f46d1d
Girish Shanmugam <s.girishshanmugam@gmail.com>
2018-04-04 15:56:30 +0530

    changed yield function

--- flipkart_tshirts_men/items.py
+++ flipkart_tshirts_men/items.py
@@ -12,3 +12,9 @@ class FlipkartTshirtsMenItem(scrapy.Item):
     # define the fields for your item here like:
     file_name = scrapy.Field()
     image_urls = scrapy.Field()
+    id = scrapy.Field()
+    flipkart_product_id = scrapy.Field()
+    title = scrapy.Field()
+    key_specs = scrapy.Field()
+    analytics_data = scrapy.Field()
+    rating = scrapy.Field()
--- flipkart_tshirts_men/spiders/tshirt_men.py
+++ flipkart_tshirts_men/spiders/tshirt_men.py
@@ -5,7 +5,7 @@ import json, csv
 
 class QuotesSpider(scrapy.Spider):
     name = "flipkart_men_tshirts"
-    start_urls = ['https://www.flipkart.com/men/tshirts/pr?otracker=nmenu_sub_Men_0_T-Shirts&page=2&sid=2oq%2Cs9b%2Cj9y&viewType=grid']
+    start_urls = ['https://www.flipkart.com/men/tshirts/pr?otracker=nmenu_sub_Men_0_T-Shirts&page=49&sid=2oq%2Cs9b%2Cj9y&viewType=grid']
     item_id = 1
 
     def parse(self, response):
@@ -22,33 +22,27 @@ class QuotesSpider(scrapy.Spider):
             raw_json = self.convert_keys_to_string(raw_json)
 
             # write the crawled data to a csv file
-            with open('result.csv', 'a') as csvfile:
-                fieldnames = ['id', 'flipkart_product_id', 'title', 'key_specs', 'analytics_data', 'rating',
-                              'file_name', 'url']
-                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-                writer.writeheader()
-                for product in raw_json['productSummary']:
-                    try:
-                        images_list = raw_json['productSummary'][product]['value']['media']['images']
-                        flipkart_item_id = raw_json['productSummary'][product]['value']['itemId']
-                        title = raw_json['productSummary'][product]['value']['titles']['title']
-                        key_specs = raw_json['productSummary'][product]['value']['keySpecs']
-                        analytics_data = raw_json['productSummary'][product]['value']['analyticsData']
-                        rating = raw_json['productSummary'][product]['value']['rating']
-                        for image in images_list:
-                            image = self.convert_keys_to_string(image)
-                            # replace the width, height and quality fields in the url
-                            image_url = image['url'].encode('utf-8').replace('{@width}', '200')
-                            image_url = image_url.encode('utf-8').replace('{@height}', '200')
-                            image_url = image_url.encode('utf-8').replace('{@quality}', '100')
-                            writer.writerow({'id': self.item_id, 'flipkart_product_id': flipkart_item_id, 'title': title,
-                                             'key_specs': key_specs, 'analytics_data': analytics_data, 'rating': rating,
-                                             'file_name': str(self.item_id)+'.jpeg', 'url': image_url})
-                            # download the images from the url
-                            yield FlipkartTshirtsMenItem(image_urls=[image_url], file_name=str(self.item_id)+'.jpeg')
-                            self.item_id = self.item_id+1
-                    except Exception as e:
-                        print product
+            for product in raw_json['productSummary']:
+                try:
+                    images_list = raw_json['productSummary'][product]['value']['media']['images']
+                    flipkart_item_id = raw_json['productSummary'][product]['value']['itemId']
+                    title = raw_json['productSummary'][product]['value']['titles']['title']
+                    key_specs = raw_json['productSummary'][product]['value']['keySpecs']
+                    analytics_data = raw_json['productSummary'][product]['value']['analyticsData']
+                    rating = raw_json['productSummary'][product]['value']['rating']
+                    for image in images_list:
+                        image = self.convert_keys_to_string(image)
+                        # replace the width, height and quality fields in the url
+                        image_url = image['url'].encode('utf-8').replace('{@width}', '200')
+                        image_url = image_url.encode('utf-8').replace('{@height}', '200')
+                        image_url = image_url.encode('utf-8').replace('{@quality}', '100')
+                        # download the images from the url
+                        yield FlipkartTshirtsMenItem(image_urls=[image_url], file_name=str(self.item_id)+'.jpeg',
+                                                     id=self.item_id, flipkart_product_id=flipkart_item_id, title=title,
+                                                     key_specs=key_specs, analytics_data=analytics_data, rating=rating)
+                        self.item_id = self.item_id+1
+                except Exception as e:
+                    print product
 
             # check if next page is available
             next_page = response.css('div._2kUstJ a::attr(href)').extract()[-1]
